0c67756e4b2f3de67981e7733acd8c55a4c2cb44
K Lauer <klauer@slac.stanford.edu>
2020-04-10 11:11:56 -0700

    REL: 0.1.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
 from setuptools import setup
 
 setup(name='epics-pypdb',
-          version='0.1.4',
+          version='0.1.5',
           description="Utilities for working with EPICS PDB files",
           author='Michael Davidsaver',
           author_email='mdavidsaver@gmail.com',
